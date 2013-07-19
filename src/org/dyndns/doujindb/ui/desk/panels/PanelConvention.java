de75059b2bed5c6374e5125af08490b4398ce564
loli10K <ezomori.nozomu@gmail.com>
2013-07-19 18:53:24 +0000

    Little UI improvements

--- src/org/dyndns/doujindb/ui/desk/panels/PanelConvention.java
+++ src/org/dyndns/doujindb/ui/desk/panels/PanelConvention.java
@@ -6,7 +6,6 @@ import java.awt.dnd.DropTargetDragEvent;
 import java.awt.dnd.DropTargetDropEvent;
 import java.awt.event.*;
 import java.util.Enumeration;
-import java.util.Hashtable;
 import java.util.TooManyListenersException;
 
 import javax.swing.*;
@@ -22,7 +21,6 @@ import org.dyndns.doujindb.db.event.*;
 import org.dyndns.doujindb.db.records.Book;
 import org.dyndns.doujindb.db.records.Convention;
 import org.dyndns.doujindb.log.Level;
-import org.dyndns.doujindb.ui.desk.*;
 import org.dyndns.doujindb.ui.desk.panels.edit.*;
 import org.dyndns.doujindb.ui.desk.panels.util.RecordList;
 import org.dyndns.doujindb.ui.desk.panels.util.TabbedPaneUIEx;
@@ -35,7 +33,7 @@ public final class PanelConvention extends JPanel implements DataBaseListener, L
 	private Color backgroundColor = Core.Properties.get("org.dyndns.doujindb.ui.theme.background").asColor();
 	private Color foregroundColor = Core.Properties.get("org.dyndns.doujindb.ui.theme.color").asColor();
 	
-	private final Font font = Core.Properties.get("org.dyndns.doujindb.ui.font").asFont();
+	private final Font font = Core.Resources.Font;
 	private JLabel labelTagName;
 	private JTextField textTagName;
 	private JLabel labelWeblink;
@@ -92,9 +90,9 @@ public final class PanelConvention extends JPanel implements DataBaseListener, L
 		});
 		textAlias.getDocument().addDocumentListener(new DocumentListener()
 		{
-		    public void insertUpdate(DocumentEvent e) { }
-		    public void removeUpdate(DocumentEvent e) { }
-		    public void changedUpdate(DocumentEvent e) { }
+		    public void insertUpdate(DocumentEvent de) { }
+		    public void removeUpdate(DocumentEvent de) { }
+		    public void changedUpdate(DocumentEvent de) { }
 		});
 		panel.add(textAlias);
 		addAlias.setBorder(null);
@@ -143,14 +141,15 @@ public final class PanelConvention extends JPanel implements DataBaseListener, L
 			}
 
 		});
-		listAlias.addMouseListener(new MouseAdapter(){
-			public void mouseClicked(MouseEvent e)
+		listAlias.addMouseListener(new MouseAdapter()
+		{
+			public void mouseClicked(MouseEvent me)
 			{
-				if(e.getClickCount() == 2 && e.getButton() == MouseEvent.BUTTON1)
+				if(me.getClickCount() == 2 && me.getButton() == MouseEvent.BUTTON1)
 				{
 					try
 					{
-						String item = (String)listAlias.getModel().getElementAt(listAlias.locationToIndex(e.getPoint()));
+						String item = (String)listAlias.getModel().getElementAt(listAlias.locationToIndex(me.getPoint()));
 						textAlias.setText(item);
 						tokenConvention.removeAlias(item);
 						((DefaultListModel<String>)listAlias.getModel()).removeElement(item);
@@ -160,56 +159,75 @@ public final class PanelConvention extends JPanel implements DataBaseListener, L
 						Core.Logger.log(dbe.getMessage(), Level.ERROR);
 						dbe.printStackTrace();
 					}
-				}else
-				if(e.getButton() == MouseEvent.BUTTON3)
-				{
-					// If not item is selected don't show any popup
-					if(listAlias.getSelectedIndices().length < 1)
-						return;
-					Hashtable<String,ImageIcon> tbl = new Hashtable<String,ImageIcon>();
-					tbl.put("Delete", Core.Resources.Icons.get("JDesktop/Explorer/Delete"));
-					final PopupMenuEx pop = new PopupMenuEx("Options", tbl);
-					pop.show((Component)e.getSource(), e.getX(), e.getY());
-					new Thread(getClass().getName()+"$MouseClicked")
-					{
-						@Override
-						public void run()
-						{
-							while(pop.isValid())
-								try { sleep(1); } catch (InterruptedException ie) { ; }
-							int selected = pop.getResult();
-							switch(selected)
-							{
-							case 0:{
-								try {
-									for(Object o : listAlias.getSelectedValuesList())
-										if(o instanceof String)
-										{
-											final String item = ((String)o);
-											tokenConvention.removeAlias(item);
-											SwingUtilities.invokeLater(new Runnable()
-											{
-												@Override
-												public void run()
-												{
-													((DefaultListModel<String>)listAlias.getModel()).removeElement(item);
-													if(Core.Database.isAutocommit())
-														Core.Database.doCommit();
-												}
-											});
-										}
-								} catch (DataBaseException dbe) {
-									Core.Logger.log(dbe.getMessage(), Level.ERROR);
-									dbe.printStackTrace();
-								}
-								listAlias.validate();
-								break;
-							}
-							}
-						}
-					}.start();
+					return;
 				}
-			  }
+				checkPopup(me);
+			}
+			@Override
+			public void mousePressed(MouseEvent me) {
+				checkPopup(me);
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent me) {
+				checkPopup(me);
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent me) {
+				checkPopup(me);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent me) {
+				checkPopup(me);
+			}
+			private void checkPopup(MouseEvent me)
+			{
+				if(!me.isPopupTrigger())
+					return;
+				
+				// If not item is selected don't show any popup
+				if(listAlias.getSelectedIndices().length < 1)
+					return;
+				
+				JPopupMenu popupMenu = new JPopupMenu();
+	    		JMenuItem menuItem = new JMenuItem("Delete", Core.Resources.Icons.get("JDesktop/Explorer/Delete"));
+	    		menuItem.addActionListener(new ActionListener()
+				{
+					@Override
+					public void actionPerformed(ActionEvent ae)
+					{
+						try
+						{
+							for(Object o : listAlias.getSelectedValuesList())
+								if(o instanceof String)
+								{
+									final String item = ((String)o);
+									tokenConvention.removeAlias(item);
+									SwingUtilities.invokeLater(new Runnable()
+									{
+										@Override
+										public void run()
+										{
+											((DefaultListModel<String>)listAlias.getModel()).removeElement(item);
+											if(Core.Database.isAutocommit())
+												Core.Database.doCommit();
+										}
+									});
+								}
+						} catch (DataBaseException dbe) {
+							Core.Logger.log(dbe.getMessage(), Level.ERROR);
+							dbe.printStackTrace();
+						}
+						listAlias.validate();
+					}
+				});
+	    		menuItem.setName("delete");
+				menuItem.setActionCommand("delete");
+				popupMenu.add(menuItem);
+				popupMenu.show(me.getComponent(), me.getX(), me.getY());
+			}
 		});
 		scrollAlias = new JScrollPane(listAlias);
 		panel.add(scrollAlias);

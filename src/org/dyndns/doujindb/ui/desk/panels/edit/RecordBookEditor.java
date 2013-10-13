508b5afc036ffd7f5bc25957849f8f69d46e84a7
loli10K <ezomori.nozomu@gmail.com>
2013-10-13 22:58:39 +0200

    Add preview-mode in Book editors

--- src/org/dyndns/doujindb/ui/desk/panels/edit/RecordBookEditor.java
+++ src/org/dyndns/doujindb/ui/desk/panels/edit/RecordBookEditor.java
@@ -3,25 +3,31 @@ package org.dyndns.doujindb.ui.desk.panels.edit;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 import org.dyndns.doujindb.Core;
+import org.dyndns.doujindb.dat.RepositoryException;
 import org.dyndns.doujindb.db.DataBaseException;
 import org.dyndns.doujindb.db.Record;
 import org.dyndns.doujindb.db.containers.BookContainer;
 import org.dyndns.doujindb.db.event.DataBaseListener;
 import org.dyndns.doujindb.db.event.UpdateData;
 import org.dyndns.doujindb.db.records.Book;
+import org.dyndns.doujindb.ui.desk.WindowEx;
 import org.dyndns.doujindb.ui.desk.panels.util.*;
+import org.dyndns.doujindb.util.ImageTool;
 
 @SuppressWarnings("serial")
 public class RecordBookEditor extends JPanel implements LayoutManager, ActionListener, DataBaseListener
 {
 	private BookContainer tokenIBook;
 	private RecordList<Book> recordList;
+	private JPanel recordPreview;
+	private JScrollPane scrollRecordPreview;
 	private JTextField searchField;
 	private boolean previewToggled = false;
 	private JButton toggleList;
@@ -52,12 +58,33 @@ public class RecordBookEditor extends JPanel implements LayoutManager, ActionLis
 		toggleList.setToolTipText("Toggle List");
 		toggleList.addActionListener(this);
 		toggleList.setFocusable(false);
+		recordPreview = new JPanel();
+		recordPreview.setLayout(new WrapLayout());
+		for(final Book book : token.getBooks())
+			try {
+				JButton bookButton;
+				recordPreview.add(
+					bookButton = new JButton(
+						new ImageIcon(
+							ImageTool.read(Core.Repository.getPreview(book.getID()).getInputStream()))));
+				bookButton.addActionListener(new ActionListener()
+				{
+					@Override
+					public void actionPerformed(ActionEvent ae) {
+						Core.UI.Desktop.showRecordWindow(WindowEx.Type.WINDOW_BOOK, book);
+					}
+				});
+			} catch (IOException ioe) { }
+		scrollRecordPreview = new JScrollPane(recordPreview);
+		scrollRecordPreview.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		togglePreview = new JButton(Core.Resources.Icons.get("Desktop/Explorer/Table/View/Preview"));
 		togglePreview.setToolTipText("Toggle Preview");
 		togglePreview.addActionListener(this);
 		togglePreview.setFocusable(false);
 		super.add(searchField);
+		super.add(recordList);
 		super.add(toggleList);
+		super.add(scrollRecordPreview);
 		super.add(togglePreview);
 		super.add(recordList);
 		super.setEnabled(false);
@@ -85,12 +112,15 @@ public class RecordBookEditor extends JPanel implements LayoutManager, ActionLis
 		if(!previewToggled)
 		{
 			toggleList.setBounds(0, 0, 0, 0);
+			recordList.setBounds(0, 20, width, height - 20);
 			togglePreview.setBounds(width - 20, 0, 20, 20);
+			scrollRecordPreview.setBounds(0, 0, 0, 0);
 		} else {
 			toggleList.setBounds(width - 20, 0, 20, 20);
+			recordList.setBounds(0, 0, 0, 0);
 			togglePreview.setBounds(0, 0, 0, 0);
+			scrollRecordPreview.setBounds(0, 20, width, height - 20);
 		}
-		recordList.setBounds(0, 20, width, height - 20);
 	}
 	
 	@Override
@@ -100,12 +130,14 @@ public class RecordBookEditor extends JPanel implements LayoutManager, ActionLis
 		{
 			previewToggled = false;
 			doLayout();
+			toggleList.validate();
 			return;
 		}
 		if(ae.getSource().equals(togglePreview))
 		{
 			previewToggled = true;
 			doLayout();
+			scrollRecordPreview.validate();
 			return;
 		}
 	}

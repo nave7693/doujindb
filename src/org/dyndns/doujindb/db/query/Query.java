f2863316aef99e7ac2d2985e08efa3eb8ecc4ad4
loli10K <ezomori.nozomu@gmail.com>
2014-05-05 22:26:07 +0200

    Add more logging information in DataBase queries classes

--- src/org/dyndns/doujindb/db/query/Query.java
+++ src/org/dyndns/doujindb/db/query/Query.java
@@ -1,7 +1,29 @@
 package org.dyndns.doujindb.db.query;
 
+import java.lang.reflect.*;
+
 import org.dyndns.doujindb.db.Record;
 
-public abstract class Query<T extends Record> {
-
+public abstract class Query<T extends Record>
+{
+	@Override
+	public String toString() {
+		boolean firstField = true;
+		String str = "[";
+		for(Field field : getClass().getFields())
+			try {
+				Object value = field.get(this);
+				if(value != null)
+				{
+					str += (firstField ? "" : ", ") + field.getName() + "='" + value + "'";
+					firstField = false;
+				}
+			} catch (IllegalArgumentException iae) {
+				iae.printStackTrace();
+			} catch (IllegalAccessException iae) {
+				iae.printStackTrace();
+			}
+		str += "]";
+		return str;
+	}
 }

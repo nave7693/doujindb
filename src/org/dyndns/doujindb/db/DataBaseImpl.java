094d789b7e1676dcb5d7a1688f11c4b3ef8c744c
loli10K <ezomori.nozomu@gmail.com>
2014-06-08 11:10:15 +0200

    Modify SelectQuery from c45f47336beb234b68badc14218ad0c5d24e0ec7 to use an OUTER JOIN

--- src/org/dyndns/doujindb/db/DataBaseImpl.java
+++ src/org/dyndns/doujindb/db/DataBaseImpl.java
@@ -110,7 +110,7 @@ final class DataBaseImpl extends IDataBase
 		         new ExpressionParameter("ID")));
 		List<Expression> conventionT = new ArrayList<Expression>();
 		conventionT.add(ExpressionFactory.likeExp("tagName", new ExpressionParameter("TagName")));
-		conventionT.add(ExpressionFactory.likeExp("aliases.tagName", new ExpressionParameter("TagName")));
+		conventionT.add(ExpressionFactory.likeExp("aliases+.tagName", new ExpressionParameter("TagName")));
 		list.add(ExpressionFactory.joinExp(Expression.OR, conventionT));
 		list.add(ExpressionFactory.likeExp("weblink", 
 		         new ExpressionParameter("Weblink")));
@@ -123,7 +123,7 @@ final class DataBaseImpl extends IDataBase
 		         new ExpressionParameter("ID")));
 		List<Expression> contentT = new ArrayList<Expression>();
 		contentT.add(ExpressionFactory.likeExp("tagName", new ExpressionParameter("TagName")));
-		contentT.add(ExpressionFactory.likeExp("aliases.tagName", new ExpressionParameter("TagName")));
+		contentT.add(ExpressionFactory.likeExp("aliases+.tagName", new ExpressionParameter("TagName")));
 		list.add(ExpressionFactory.joinExp(Expression.OR, contentT));
 		list.add(Expression.fromString("recycled = FALSE"));
 		exp = ExpressionFactory.joinExp(Expression.AND, list);

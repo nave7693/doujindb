eb848b2b6a16290c4361caea612f9451cce47c0b
loli10K <ezomori.nozomu@gmail.com>
2014-06-08 11:27:30 +0200

    Add QueryType (AND, OR) enum

--- src/org/dyndns/doujindb/db/query/Query.java
+++ src/org/dyndns/doujindb/db/query/Query.java
@@ -6,6 +6,19 @@ import org.dyndns.doujindb.db.Record;
 
 public abstract class Query<T extends Record>
 {
+	public enum Type
+	{
+		OR,
+		AND
+	}
+	
+	public Type QueryType;
+	
+	protected Query()
+	{
+		QueryType = Type.AND;
+	}
+	
 	@Override
 	public String toString() {
 		boolean firstField = true;

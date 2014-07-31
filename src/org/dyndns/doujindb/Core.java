767f4e3e0cbe20ab839b22bcef9fd08070256e99
loli10K <ezomori.nozomu@gmail.com>
2014-07-31 12:50:20 +0200

    Rewrite logging system used in Core from custom to Apache's commons-logging. Add new logging properties to system Configuration

--- src/org/dyndns/doujindb/Core.java
+++ src/org/dyndns/doujindb/Core.java
@@ -3,8 +3,9 @@ package org.dyndns.doujindb;
 import java.beans.PropertyVetoException;
 import java.io.*;
 
+import org.apache.commons.logging.*;
+
 import org.dyndns.doujindb.conf.*;
-import org.dyndns.doujindb.log.*;
 import org.dyndns.doujindb.plug.PluginManager;
 import org.dyndns.doujindb.ui.*;
 import org.dyndns.doujindb.ui.dialog.DialogConfigurationWizard;
@@ -18,19 +19,30 @@ public final class Core implements Runnable
 {
 	public final static File DOUJINDB_HOME = getHomedir();
 	
-	private static final String TAG = "Core : ";
+	private static final Log Logger = getLogger();
 	
 	private static File getHomedir()
 	{
 		File homedir;
-		try
-		{
+		try {
 			homedir = new File(java.net.URLDecoder.decode(Core.class.getProtectionDomain().getCodeSource().getLocation().getPath(), "UTF-8")).getParentFile();
-		} catch (UnsupportedEncodingException uee)
-		{
+		} catch (UnsupportedEncodingException uee) {
 			homedir = new File(Core.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getParentFile();
 		}
 		return homedir;
+	}
+	
+	private static Log getLogger()
+	{
+		// setup default Apache's commons-logging implementation
+		System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
+		System.setProperty("org.apache.commons.logging.simplelog.defaultlog", "info");
+		System.setProperty("org.apache.commons.logging.simplelog.showlogname", "false");
+		System.setProperty("org.apache.commons.logging.simplelog.showShortLogname", "true");
+		System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
+		System.setProperty("org.apache.commons.logging.simplelog.dateTimeFormat", "yyyy-MM-dd'T'HH:mm:ss'Z'");
+		// return default logger instance
+		return LogFactory.getLog(Core.class);
 	}
 
 	static
@@ -46,48 +58,90 @@ public final class Core implements Runnable
 	public void run()
 	{
 		Thread.currentThread().setName("core-bootstrap");
-		Logger.logInfo(TAG + "bootstrap started.");
+		Logger.info("Core bootstrap started");
 		
+		/**
+		 * Load all Plugins without starting them:
+		 *    some Plugins may need the DataBase and/or DataStore online to be started
+		 *    also Configuration properties have yet to be loaded
+		 */
 		PluginManager.loadAll();
 		
+		/**
+		 * Load global Configuration properties
+		 * In case of Exception (e.g missing configuration file on first run) schedule the Configuration Wizard
+		 */
 		boolean isConfigurationWizard = false;
-		try
-		{
-			Logger.logInfo(TAG + "loading system configuration ...");
+		try {
 			Configuration.configLoad();
-			Logger.logInfo(TAG + "system configuration loaded.");
-			Logger.enableDebug((boolean) Configuration.configRead("org.dyndns.doujindb.log.debug"));
-		} catch (ConfigurationException ce)
-		{
-			Logger.logError(TAG + "failed to load system configuration.", ce);
-			Logger.logInfo(TAG + "Configuration Wizard scheduled to run on startup.");
+		} catch (ConfigurationException ce) {
+			Logger.error("Error loading system Configuration", ce);
+			Logger.info("Scheduling Configuration Wizard");
 			isConfigurationWizard = true;
 		}
-		if(java.awt.GraphicsEnvironment.isHeadless()) 
-		{
-			Logger.logFatal(TAG + "DoujinDB cannot run on headless systems.");
-			System.exit(-1);
+		
+		/**
+		 * We need AWT/Swing (graphical user interface) to run DoujinDB
+		 * Quit if it's not available (running "Headless")
+		 */
+		if(java.awt.GraphicsEnvironment.isHeadless()) {
+			Logger.fatal("DoujinDB cannot run on headless systems");
+			System.exit(1);
 		}
 		
+		//FIXME should be checked by the DataStore itself
 		if(Configuration.configRead("org.dyndns.doujindb.dat.datastore").equals(Configuration.configRead("org.dyndns.doujindb.dat.temp")))
-			Logger.logWarning(TAG + "datastore directory is set to the temporary system directory.");
+			Logger.warn("datastore directory is set to the temporary system directory");
 		
-		if(!((Boolean)Configuration.configRead("org.dyndns.doujindb.log.cayenne")))
-			System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
+		/**
+		 * Load and setup Logging related Configuration
+		 * Defaults to level 'info' in case of any Exception
+		 */
+		String baseCommonsLogging = "org.apache.commons.logging.simplelog.log.";
+		String baseConfiguration = "org.dyndns.doujindb.log.";
+		String[] configurationSet = new String[]{
+			"org.apache.cayenne",
+			"org.dyndns.doujindb.conf",
+			"org.dyndns.doujindb.dat",
+			"org.dyndns.doujindb.db",
+			"org.dyndns.doujindb.plug",
+			"org.dyndns.doujindb.ui",
+			"org.dyndns.doujindb.util",
+			"org.dyndns.doujindb.net"
+		};
+		for(String configurationKey : configurationSet) {
+			try {
+				String level = (String) Configuration.configRead(baseConfiguration + configurationKey);
+				System.setProperty(baseCommonsLogging + configurationKey, level);
+				Logger.info(String.format("Configured log level [%s] for [%s]", level, configurationKey));
+			} catch (Exception e) {
+				Logger.error(String.format("Error loading logging configuration level for [%s]", configurationKey), e);
+				Logger.info(String.format("Configuring log level [%s] for [%s]", "info", configurationKey));
+				System.setProperty(baseCommonsLogging + configurationKey, "info");
+			}
+		}
 		
-		Logger.logInfo(TAG + "loading user interface ...");
+		/**
+		 * Start the main User Interface
+		 * We don't need to keep any reference to it, all public methods should be static anyway
+		 */
+		//TODO Catch Exceptions
 		new UI();
-		Logger.logInfo(TAG + "user interface loaded.");
 		
+		/**
+		 * Now that we have up and running Logging, Configuration and User Interface we can manually start every Plugin instance
+		 * //FIXME Plugins should register listeners for DataBase/DataStore/UI/Configuration and decide for themselves when to "start"
+		 */
 		PluginManager.startAll();
 		
-		if(isConfigurationWizard)
-		{
-			Logger.logInfo(TAG + "running Configuration Wizard ...");
+		/**
+		 * Run now the Configuration Wizard if an Exception occurred during Configuration load
+		 */
+		if(isConfigurationWizard) {
 			try {
 				UI.Desktop.showDialog(new DialogConfigurationWizard());
 			} catch (PropertyVetoException pve) {
-				Logger.logWarning(pve.getMessage(), pve);
+				Logger.error("Error loading Configuration Wizard", pve);
 			}
 		}
 	}
--- src/org/dyndns/doujindb/conf/Configuration.java
+++ src/org/dyndns/doujindb/conf/Configuration.java
@@ -25,29 +25,35 @@ public final class Configuration
 	static
 	{
 		Logger.logInfo(TAG + "initializing ...");
-		configAdd("org.dyndns.doujindb.ui.font",               "<html><body>Default JCK font.<br/>Used to render Japanese/Chinese/Korean strings.</body></html>", new Font("Dialog.plain", Font.PLAIN, 11));
-		configAdd("org.dyndns.doujindb.ui.font_size",          "<html><body>Default font size.</body></html>", 11);
-		configAdd("org.dyndns.doujindb.ui.always_on_top",      "<html><body>Whether the user interface should be always painted on top of other windows.</html>", false);
-		configAdd("org.dyndns.doujindb.ui.tray_on_exit",       "<html><body>Whether the user interface should be minimized on tray when is closed.</body></html>", false);
-		configAdd("org.dyndns.doujindb.ui.theme.color",        "<html><body>Foreground windows color.</body></html>", new Color(0xAA, 0xAA, 0xAA));
-		configAdd("org.dyndns.doujindb.ui.theme.background",   "<html><body>Background windows color.</body></html>", new Color(0x22, 0x22, 0x22));
-		configAdd("org.dyndns.doujindb.ui.book_preview",      "<html><body>Enable Book image preview</body></html>", true);
-		configAdd("org.dyndns.doujindb.dat.datastore",         "<html><body>The folder in which are stored all the media files.</body></html>", System.getProperty("java.io.tmpdir"));
-		configAdd("org.dyndns.doujindb.dat.file_extension",    "<html><body>Default file extension given to files when exporting media archives.</body></html>", ".zip");
-		configAdd("org.dyndns.doujindb.dat.temp",              "<html><body>Temporary folder used to store session media files.</body></html>", System.getProperty("java.io.tmpdir"));
-		configAdd("org.dyndns.doujindb.dat.keep_cache",        "<html><body>Keep a local cache of cover files.</body></html>", true);
-		configAdd("org.dyndns.doujindb.dat.cache_dir",         "<html><body>Local cover cache directory.</body></html>", System.getProperty("java.io.tmpdir"));
-//		configAdd("org.dyndns.doujindb.net.listen_port",       "<html><body>Network port used to accept incoming connections.</body></html>", 1099);
-		configAdd("org.dyndns.doujindb.sys.check_updates",     "<html><body>Check if updates are available.</body></html>", true);
-		configAdd("org.dyndns.doujindb.log.debug",             "<html><body>Log debug messages.</body></html>", false);
-		configAdd("org.dyndns.doujindb.log.cayenne",           "<html><body>Cayenne logging.</body></html>", false);
-		configAdd("org.dyndns.doujindb.db.driver",             "<html><body>SQL Driver full qualified class name.</body></html>", "sql.jdbc.Driver");
-		configAdd("org.dyndns.doujindb.db.url",                "<html><body>SQL Connection URL</body></html>", "jdbc:sql://localhost/db");
-		configAdd("org.dyndns.doujindb.db.username",           "<html><body>Database username.</body></html>", "");
-		configAdd("org.dyndns.doujindb.db.password",           "<html><body>Database password.</body></html>", "");
-		configAdd("org.dyndns.doujindb.db.connection_timeout", "<html><body>JDBC connection timeout in seconds.</body></html>", 5);
-//		configAdd("org.dyndns.doujindb.plugin.discovery",        "<html><body>Automatic Plugin discovery.</body></html>", false);
-//		configAdd("org.dyndns.doujindb.plugin.timeout",          "<html><body>Timeout (in seconds) which PluginManager will wait for plugins to start/stop.</body></html>", 60);
+		configAdd("org.dyndns.doujindb.ui.font",                      "<html><body>Default JCK font.<br/>Used to render Japanese/Chinese/Korean strings.</body></html>", new Font("Dialog.plain", Font.PLAIN, 11));
+		configAdd("org.dyndns.doujindb.ui.font_size",                 "<html><body>Default font size.</body></html>", 11);
+		configAdd("org.dyndns.doujindb.ui.always_on_top",             "<html><body>Whether the user interface should be always painted on top of other windows.</html>", false);
+		configAdd("org.dyndns.doujindb.ui.tray_on_exit",              "<html><body>Whether the user interface should be minimized on tray when is closed.</body></html>", false);
+		configAdd("org.dyndns.doujindb.ui.theme.color",               "<html><body>Foreground windows color.</body></html>", new Color(0xAA, 0xAA, 0xAA));
+		configAdd("org.dyndns.doujindb.ui.theme.background",          "<html><body>Background windows color.</body></html>", new Color(0x22, 0x22, 0x22));
+		configAdd("org.dyndns.doujindb.ui.book_preview",              "<html><body>Enable Book image preview</body></html>", true);
+		configAdd("org.dyndns.doujindb.dat.datastore",                "<html><body>The folder in which are stored all the media files.</body></html>", System.getProperty("java.io.tmpdir"));
+		configAdd("org.dyndns.doujindb.dat.file_extension",           "<html><body>Default file extension given to files when exporting media archives.</body></html>", ".zip");
+		configAdd("org.dyndns.doujindb.dat.temp",                     "<html><body>Temporary folder used to store session media files.</body></html>", System.getProperty("java.io.tmpdir"));
+		configAdd("org.dyndns.doujindb.dat.keep_cache",               "<html><body>Keep a local cache of cover files.</body></html>", true);
+		configAdd("org.dyndns.doujindb.dat.cache_dir",                "<html><body>Local cover cache directory.</body></html>", System.getProperty("java.io.tmpdir"));
+//		configAdd("org.dyndns.doujindb.net.listen_port",              "<html><body>Network port used to accept incoming connections.</body></html>", 1099);
+		configAdd("org.dyndns.doujindb.sys.check_updates",            "<html><body>Check if updates are available.</body></html>", true);
+		configAdd("org.dyndns.doujindb.db.driver",                    "<html><body>SQL Driver full qualified class name.</body></html>", "sql.jdbc.Driver");
+		configAdd("org.dyndns.doujindb.db.url",                       "<html><body>SQL Connection URL</body></html>", "jdbc:sql://localhost/db");
+		configAdd("org.dyndns.doujindb.db.username",                  "<html><body>Database username.</body></html>", "");
+		configAdd("org.dyndns.doujindb.db.password",                  "<html><body>Database password.</body></html>", "");
+		configAdd("org.dyndns.doujindb.db.connection_timeout",        "<html><body>JDBC connection timeout in seconds.</body></html>", 5);
+//		configAdd("org.dyndns.doujindb.plugin.discovery",             "<html><body>Automatic Plugin discovery.</body></html>", false);
+//		configAdd("org.dyndns.doujindb.plugin.timeout",               "<html><body>Timeout (in seconds) which PluginManager will wait for plugins to start/stop.</body></html>", 60);
+		configAdd("org.dyndns.doujindb.log.org.dyndns.doujindb.conf", "<html><body>Log LEVEL for doujindb configuration.</body></html>", "info");
+		configAdd("org.dyndns.doujindb.log.org.dyndns.doujindb.dat",  "<html><body>Log LEVEL for doujindb datastore.</body></html>", "info");
+		configAdd("org.dyndns.doujindb.log.org.dyndns.doujindb.db",   "<html><body>Log LEVEL for doujindb database.</body></html>", "info");
+		configAdd("org.dyndns.doujindb.log.org.dyndns.doujindb.plug", "<html><body>Log LEVEL for doujindb plugins.</body></html>", "info");
+		configAdd("org.dyndns.doujindb.log.org.dyndns.doujindb.ui",   "<html><body>Log LEVEL for doujindb user interface.</body></html>", "info");
+		configAdd("org.dyndns.doujindb.log.org.dyndns.doujindb.util", "<html><body>Log LEVEL for doujindb utility.</body></html>", "info");
+		configAdd("org.dyndns.doujindb.log.org.dyndns.doujindb.net",  "<html><body>Log LEVEL for doujindb network.</body></html>", "info");
+		configAdd("org.dyndns.doujindb.log.org.apache.cayenne",       "<html><body>Log LEVEL for cayenne.</body></html>", "info");
 	}
 
 	public static Object configRead(String key) throws ConfigurationException

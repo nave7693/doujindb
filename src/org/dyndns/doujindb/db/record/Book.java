ea2f7d0a802627712de8862ced56551a7480a89f
loli10K <ezomori.nozomu@gmail.com>
2014-08-17 07:59:33 +0200

    Bump Book rating range from 1-5 to 1-10

--- src/org/dyndns/doujindb/db/record/Book.java
+++ src/org/dyndns/doujindb/db/record/Book.java
@@ -21,7 +21,7 @@ public interface Book extends Record, ArtistContainer, CircleContainer, ContentC
 	
 	public enum Rating implements org.apache.cayenne.ExtendedEnumeration
 	{
-		UNRATED(0), R1(1), R2(2), R3(3), R4(4), R5(5);
+		UNRATED(0), R1(1), R2(2), R3(3), R4(4), R5(5), R6(6), R7(7), R8(8), R9(9), R10(10);
 		
 		private Integer value;
 

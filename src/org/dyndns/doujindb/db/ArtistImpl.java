nave7693/doujindb

commit 68f02d2725282da79cf1253ee1148071adb5ff1d

Delete RecordImpl.class

src/org/dyndns/doujindb/db/ArtistImpl.java
@@ -6,7 +6,7 @@ import java.util.*;
 import org.dyndns.doujindb.db.event.UpdateData;
 import org.dyndns.doujindb.db.records.*;
 
-final class ArtistImpl extends RecordImpl implements Artist, Serializable
+final class ArtistImpl extends org.dyndns.doujindb.db.cayenne.Artist implements Artist, Serializable
 {
 	private static final long serialVersionUID = 0xFEED0001L;
 	

src/org/dyndns/doujindb/db/BookImpl.java
@@ -6,7 +6,7 @@ import java.util.*;
 import org.dyndns.doujindb.db.event.UpdateData;
 import org.dyndns.doujindb.db.records.*;
 
-final class BookImpl extends RecordImpl implements Book, Serializable
+final class BookImpl extends org.dyndns.doujindb.db.cayenne.Book implements Book, Serializable
 {
 	private static final long serialVersionUID = 0xFEED0001L;
 	

src/org/dyndns/doujindb/db/CircleImpl.java
@@ -6,7 +6,7 @@ import java.util.*;
 import org.dyndns.doujindb.db.event.UpdateData;
 import org.dyndns.doujindb.db.records.*;
 
-final class CircleImpl extends RecordImpl implements Circle, Serializable
+final class CircleImpl extends org.dyndns.doujindb.db.cayenne.Circle implements Circle, Serializable
 {
 	private static final long serialVersionUID = 0xFEED0001L;
 

src/org/dyndns/doujindb/db/ContentImpl.java
@@ -6,7 +6,7 @@ import java.util.*;
 import org.dyndns.doujindb.db.event.UpdateData;
 import org.dyndns.doujindb.db.records.*;
 
-final class ContentImpl extends RecordImpl implements Content, Serializable
+final class ContentImpl extends org.dyndns.doujindb.db.cayenne.Content implements Content, Serializable
 {
 	private static final long serialVersionUID = 0xFEED0001L;
 

src/org/dyndns/doujindb/db/ConventionImpl.java
@@ -6,7 +6,7 @@ import java.util.*;
 import org.dyndns.doujindb.db.event.UpdateData;
 import org.dyndns.doujindb.db.records.*;
 
-final class ConventionImpl extends RecordImpl implements Convention, Serializable
+final class ConventionImpl extends org.dyndns.doujindb.db.cayenne.Convention implements Convention, Serializable
 {
 	private static final long serialVersionUID = 0xFEED0001L;
 

src/org/dyndns/doujindb/db/DataBaseImpl.java
@@ -278,7 +278,7 @@ final class DataBaseImpl extends IDataBase
 	@Override
 	public synchronized void doDelete(Record record) throws DataBaseException
 	{
-		context.deleteObject(((RecordImpl)record).ref);
+		context.deleteObject(record);
 		
 		DataBase.fireRecordDeleted(record);
 	}

src/org/dyndns/doujindb/db/ParodyImpl.java
@@ -6,7 +6,7 @@ import java.util.*;
 import org.dyndns.doujindb.db.event.UpdateData;
 import org.dyndns.doujindb.db.records.*;
 
-final class ParodyImpl extends RecordImpl implements Parody, Serializable
+final class ParodyImpl extends org.dyndns.doujindb.db.cayenne.Parody implements Parody, Serializable
 {
 	private static final long serialVersionUID = 0xFEED0001L;
 

src/org/dyndns/doujindb/db/RecordImpl.java
@@ -1,16 +1,0 @@
-package org.dyndns.doujindb.db;
-
-import java.io.Serializable;
-
-import org.apache.cayenne.CayenneDataObject;
-
-@SuppressWarnings("serial")
-abstract class RecordImpl implements Record, Serializable
-{
-	protected CayenneDataObject ref;
-
-	public RecordImpl() throws DataBaseException
-	{
-		super();
-	}
-}

commit ee75b4b25ded0fd803b75a5072c43d019cf50700

Restore DataBaseImpl.doDelete() functionality (broken in 68f02d27)

src/org/dyndns/doujindb/db/ArtistImpl.java
@@ -2,10 +2,12 @@ package org.dyndns.doujindb.db;
 
 import java.util.*;
 
+import org.apache.cayenne.CayenneDataObject;
 import org.dyndns.doujindb.db.event.UpdateData;
 import org.dyndns.doujindb.db.record.*;
 
-final class ArtistImpl implements Artist
+@SuppressWarnings("serial")
+final class ArtistImpl extends RecordImpl implements Artist
 {
 	org.dyndns.doujindb.db.cayenne.Artist ref;
 	
@@ -233,5 +235,10 @@ final class ArtistImpl implements Artist
 			return false;
 		else
 			return compareTo((Artist)obj) == 0;
+	}
+
+	@Override
+	protected CayenneDataObject getRef() {
+		return ref;
 	}
 }

src/org/dyndns/doujindb/db/BookImpl.java
@@ -2,10 +2,12 @@ package org.dyndns.doujindb.db;
 
 import java.util.*;
 
+import org.apache.cayenne.CayenneDataObject;
 import org.dyndns.doujindb.db.event.UpdateData;
 import org.dyndns.doujindb.db.record.*;
 
-final class BookImpl implements Book
+@SuppressWarnings("serial")
+final class BookImpl extends RecordImpl implements Book
 {
 	org.dyndns.doujindb.db.cayenne.Book ref;
 	
@@ -419,5 +421,10 @@ final class BookImpl implements Book
 			return false;
 		else
 			return compareTo((Book)obj) == 0;
+	}
+
+	@Override
+	protected CayenneDataObject getRef() {
+		return ref;
 	}
 }

src/org/dyndns/doujindb/db/CircleImpl.java
@@ -2,10 +2,12 @@ package org.dyndns.doujindb.db;
 
 import java.util.*;
 
+import org.apache.cayenne.CayenneDataObject;
 import org.dyndns.doujindb.db.event.UpdateData;
 import org.dyndns.doujindb.db.record.*;
 
-final class CircleImpl implements Circle
+@SuppressWarnings("serial")
+final class CircleImpl extends RecordImpl implements Circle
 {
 	org.dyndns.doujindb.db.cayenne.Circle ref;
 
@@ -234,5 +236,10 @@ final class CircleImpl implements Circle
 			return false;
 		else
 			return compareTo((Circle)obj) == 0;
+	}
+
+	@Override
+	protected CayenneDataObject getRef() {
+		return ref;
 	}
 }

src/org/dyndns/doujindb/db/ContentImpl.java
@@ -2,10 +2,12 @@ package org.dyndns.doujindb.db;
 
 import java.util.*;
 
+import org.apache.cayenne.CayenneDataObject;
 import org.dyndns.doujindb.db.event.UpdateData;
 import org.dyndns.doujindb.db.record.*;
 
-final class ContentImpl implements Content
+@SuppressWarnings("serial")
+final class ContentImpl extends RecordImpl implements Content
 {
 	org.dyndns.doujindb.db.cayenne.Content ref;
 
@@ -181,5 +183,10 @@ final class ContentImpl implements Content
 			return;
 		ref.setNamespace(namespace);
 		DataBase.fireRecordUpdated(this, UpdateData.property("namespace"));
+	}
+
+	@Override
+	protected CayenneDataObject getRef() {
+		return ref;
 	}
 }

src/org/dyndns/doujindb/db/ConventionImpl.java
@@ -2,10 +2,12 @@ package org.dyndns.doujindb.db;
 
 import java.util.*;
 
+import org.apache.cayenne.CayenneDataObject;
 import org.dyndns.doujindb.db.event.UpdateData;
 import org.dyndns.doujindb.db.record.*;
 
-final class ConventionImpl implements Convention
+@SuppressWarnings("serial")
+final class ConventionImpl extends RecordImpl implements Convention
 {
 	org.dyndns.doujindb.db.cayenne.Convention ref;
 
@@ -181,5 +183,10 @@ final class ConventionImpl implements Convention
 			return false;
 		else
 			return compareTo((Convention)obj) == 0;
+	}
+
+	@Override
+	protected CayenneDataObject getRef() {
+		return ref;
 	}
 }

src/org/dyndns/doujindb/db/DataBaseImpl.java
@@ -306,7 +306,7 @@ final class DataBaseImpl extends IDataBase
 	@Override
 	public synchronized void doDelete(Record record) throws DataBaseException
 	{
-		context.deleteObject(record);
+		context.deleteObject(((RecordImpl)record).getRef());
 		
 		DataBase.fireRecordDeleted(record);
 	}

src/org/dyndns/doujindb/db/ParodyImpl.java
@@ -2,10 +2,12 @@ package org.dyndns.doujindb.db;
 
 import java.util.*;
 
+import org.apache.cayenne.CayenneDataObject;
 import org.dyndns.doujindb.db.event.UpdateData;
 import org.dyndns.doujindb.db.record.*;
 
-final class ParodyImpl implements Parody
+@SuppressWarnings("serial")
+final class ParodyImpl extends RecordImpl implements Parody
 {
 	org.dyndns.doujindb.db.cayenne.Parody ref;
 	
@@ -199,5 +201,10 @@ final class ParodyImpl implements Parody
 			return false;
 		else
 			return compareTo((Parody)obj) == 0;
+	}
+
+	@Override
+	protected CayenneDataObject getRef() {
+		return ref;
 	}
 }

src/org/dyndns/doujindb/db/RecordImpl.java
@@ -1,0 +1,16 @@
+package org.dyndns.doujindb.db;
+
+import java.io.Serializable;
+
+import org.apache.cayenne.CayenneDataObject;
+
+@SuppressWarnings("serial")
+abstract class RecordImpl implements Record, Serializable
+{
+	public RecordImpl() throws DataBaseException
+	{
+		super();
+	}
+	
+	abstract protected CayenneDataObject getRef();
+}
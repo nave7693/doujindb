63c71812e6f58540b5517310fe5c534ea16ade6a
loli10K <ezomori.nozomu@gmail.com>
2014-10-10 11:31:07 +0200

    Add Namespace parsing in SearchComboBox auto-suggest mode

--- src/org/dyndns/doujindb/ui/dialog/util/combobox/ComboBoxContent.java
+++ src/org/dyndns/doujindb/ui/dialog/util/combobox/ComboBoxContent.java
@@ -7,6 +7,7 @@ import javax.swing.*;
 import org.dyndns.doujindb.db.*;
 import org.dyndns.doujindb.db.query.QueryContent;
 import org.dyndns.doujindb.db.record.Content;
+import org.dyndns.doujindb.db.record.Content.Namespace;
 
 @SuppressWarnings("serial")
 public class ComboBoxContent extends SearchComboBox<Content>
@@ -36,7 +37,26 @@ public class ComboBoxContent extends SearchComboBox<Content>
 	{
 	    DefaultComboBoxModel<Content> mdl = new DefaultComboBoxModel<Content>();
 	    QueryContent qc = new QueryContent();
-	    qc.TagName = text + "%";
+	    if(text.contains(":")) {
+	    	// This query contains a Namespace specified as "ns:content"
+	    	// parse the 'text' string accordingly to build the query
+	    	// also Capitalize the first letter in Namespace string
+	    	String ns = text.substring(0, text.indexOf(':'));
+	    	ns = ns.substring(0,1).toUpperCase() + ns.substring(1);
+	    	String tname = text.substring(text.indexOf(':') + 1);
+	    	// try to parse 'ns' string to Namespace type
+	    	try {
+	    		qc.Namespace = Namespace.valueOf(ns);
+	    	} catch (IllegalArgumentException iae) {
+	    		// 'ns' is not a valid Namespace
+	    		// query returns automatically 0 rows
+	    		 return mdl;
+	    	}
+	    	qc.TagName = tname + "%";
+	    } else {
+	    	// Simple text content query, no Namespace involved
+	    	qc.TagName = text + "%";
+	    }
 	    for(Content e : DataBase.getContents(qc)) {
 	    	mdl.addElement(e);
 	    }

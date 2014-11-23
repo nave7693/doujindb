2f534a14190207fd9dc8f95f5c248bd295a8cdbb
loli10K <ezomori.nozomu@gmail.com>
2014-11-23 22:07:41 +0100

    Add hashdb_threads to Configuration, adjust query_threshold default value to new hashing method

--- src/org/dyndns/doujindb/plug/impl/imagesearch/Configuration.java
+++ src/org/dyndns/doujindb/plug/impl/imagesearch/Configuration.java
@@ -4,7 +4,8 @@ import org.dyndns.doujindb.conf.ConfigurationItem;
 
 public final class Configuration
 {
-	public static final ConfigurationItem<Integer> query_threshold = new ConfigurationItem<Integer>(75, "Threshold limit for matching image queries");
+	public static final ConfigurationItem<Integer> query_threshold = new ConfigurationItem<Integer>(3, "Threshold limit for matching image queries");
 	public static final ConfigurationItem<Integer> query_maxresult = new ConfigurationItem<Integer>(25, "Max results returned by a single image search");
 	public static final ConfigurationItem<Integer> cache_imagescale = new ConfigurationItem<Integer>(16, "Scaling factor of cover image in index file");
+	public static final ConfigurationItem<Integer> hashdb_threads = new ConfigurationItem<Integer>(1, "Concurrent threads used to build hash database");
 }

bf0f819e39df8a8e1d999a74f24f5f5d3fb5cbe8
loli10K <ezomori.nozomu@gmail.com>
2014-11-24 20:37:58 +0100

    Add ConfigurationItem Validator to hashdb_threads

--- src/org/dyndns/doujindb/plug/impl/imagesearch/Configuration.java
+++ src/org/dyndns/doujindb/plug/impl/imagesearch/Configuration.java
@@ -1,11 +1,17 @@
 package org.dyndns.doujindb.plug.impl.imagesearch;
 
 import org.dyndns.doujindb.conf.ConfigurationItem;
+import org.dyndns.doujindb.conf.ConfigurationItem.Validator;
 
 public final class Configuration
 {
 	public static final ConfigurationItem<Integer> query_threshold = new ConfigurationItem<Integer>(3, "Threshold limit for matching image queries");
 	public static final ConfigurationItem<Integer> query_maxresult = new ConfigurationItem<Integer>(25, "Max results returned by a single image search");
 	public static final ConfigurationItem<Integer> cache_imagescale = new ConfigurationItem<Integer>(16, "Scaling factor of cover image in index file");
-	public static final ConfigurationItem<Integer> hashdb_threads = new ConfigurationItem<Integer>(1, "Concurrent threads used to build hash database");
+	public static final ConfigurationItem<Integer> hashdb_threads = new ConfigurationItem<Integer>(1, 1, "Concurrent threads used to build hash database", new Validator<Integer>() {
+		@Override
+		public boolean isValid(Integer value) {
+			return value > 0 && value <= Runtime.getRuntime().availableProcessors();
+		}
+	});
 }

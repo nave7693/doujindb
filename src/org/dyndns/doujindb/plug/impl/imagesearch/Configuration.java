7e75a4ce32f48f1e8152c7a5c284ff85cf7e8010
loli10K <ezomori.nozomu@gmail.com>
2014-11-29 11:28:33 +0100

    Remove unused configuration cache_imagescale

--- src/org/dyndns/doujindb/plug/impl/imagesearch/Configuration.java
+++ src/org/dyndns/doujindb/plug/impl/imagesearch/Configuration.java
@@ -7,7 +7,6 @@ public final class Configuration
 {
 	public static final ConfigurationItem<Integer> query_threshold = new ConfigurationItem<Integer>(3, "Threshold limit for matching image queries");
 	public static final ConfigurationItem<Integer> query_maxresult = new ConfigurationItem<Integer>(25, "Max results returned by a single image search");
-	public static final ConfigurationItem<Integer> cache_imagescale = new ConfigurationItem<Integer>(16, "Scaling factor of cover image in index file");
 	public static final ConfigurationItem<Integer> hashdb_threads = new ConfigurationItem<Integer>(1, 1, "Concurrent threads used to build hash database", new Validator<Integer>() {
 		@Override
 		public boolean isValid(Integer value) {

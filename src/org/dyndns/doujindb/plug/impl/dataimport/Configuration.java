e499e76f415d13aaaaebe440d249877aed7c434a
loli10K <ezomori.nozomu@gmail.com>
2014-12-04 18:22:21 +0100

    Add HTTP connect timeout (default 15s)

--- src/org/dyndns/doujindb/plug/impl/dataimport/Configuration.java
+++ src/org/dyndns/doujindb/plug/impl/dataimport/Configuration.java
@@ -5,6 +5,7 @@ import org.dyndns.doujindb.conf.ConfigurationItem;
 public final class Configuration
 {
 	public static final ConfigurationItem<String> options_http_useragent = new ConfigurationItem<String>("Mozilla/5.0 (compatible; " + DataImport.Name+ "/" + DataImport.Version + "; +" + DataImport.Weblink + ")", "User-Agent used in HTTP requests");
+	public static final ConfigurationItem<Integer> options_http_timeout = new ConfigurationItem<Integer>(15000, "Timeout value for HTTP connections expressed in milliseconds");
 	public static final ConfigurationItem<Boolean> options_autoadd = new ConfigurationItem<Boolean>(true , "Automatically add new entries (Arist, Circle, Parody ...) to the Database without any confirmation");
 	
 	public static final ConfigurationItem<String> provider_mugimugi_apikey = new ConfigurationItem<String>("", "User KEY needed to query doujinshi.mugimugi.org API system");

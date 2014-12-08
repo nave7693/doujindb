24228937c78915040d231fa99da15c8fc84d268b
loli10K <ezomori.nozomu@gmail.com>
2014-12-08 14:55:36 +0100

    Update Task implementation

--- src/org/dyndns/doujindb/plug/impl/dataimport/Task.java
+++ src/org/dyndns/doujindb/plug/impl/dataimport/Task.java
@@ -1,73 +1,48 @@
 package org.dyndns.doujindb.plug.impl.dataimport;
 
-import java.util.Set;
+import java.io.*;
+import java.util.*;
 
 import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.adapters.*;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(namespace="org.dyndns.doujindb.plug.impl.dataimport", name="Task")
-@XmlSeeAlso({org.dyndns.doujindb.plug.impl.dataimport.TaskManager.TaskImpl.class})
-abstract class Task
+class Task
 {
 	// Needed by JAXB
 	// Define this or suffer an IllegalAnnotationsException : Task does not have a no-arg default constructor.
 	Task() { }
 	
-	Task(String id, String path)
-	{
+	Task(String id, String file) {
 		this.id = id;
-		this.path = path;
+		this.file = file;
 	}
 	
-	enum Info {
-		IDLE,		// Yet to be started
-		RUNNING,	// Still running
-		WARNING,	// Completed with warning(s)
-		ERROR,		// Automatically stopped by error(s)
-		COMPLETED,	// Completed successfully
-		PAUSED		// Waiting for user input
+	@XmlAttribute(name="id")
+	private String id = "";
+	@XmlElement(name="file")
+	private String file = "";
+	
+	@XmlElement(name="metadata")
+	public Set<Metadata> metadata = new HashSet<Metadata>();
+	
+	@XmlJavaTypeAdapter(CDATAAdapter.class)
+	public String message;
+	@XmlJavaTypeAdapter(CDATAAdapter.class)
+	public String exception;
+	
+	public void exception(Throwable t) {
+		try {
+			ByteArrayOutputStream os = new ByteArrayOutputStream();
+			PrintStream ps = new PrintStream(os);
+			t.printStackTrace(ps);
+			exception = os.toString("UTF8");
+		} catch (Exception e) {
+			exception = t.getMessage();
+		}
 	}
 	
-	enum Exec {
-		NO_OPERATION,		// Slacking off
-		CHECK_API,			// Checks if API key is set/valid
-		CHECK_DUPLICATE,	// Checks if item is a duplicate entry (cover image)
-		CHECK_SIMILARITY,	// Checks if item has a similar entry (name)
-		SAVE_DATABASE,		// Insert data into the database
-		SAVE_DATASTORE,		// Save data files in the datastore
-		SCAN_IMAGE,			// Find/resize cover image to be uploaded
-		UPLOAD_IMAGE,		// Upload image to mugimugi API system
-		PARSE_XML,			// Parse returned XML data
-		PARSE_BID,			// Add data directly from a BookID (mugimugi)
-		CLEANUP_DATA		// Housekeeping
-	}
-	
-	@XmlAttribute(name="taskId")
-	private String id;
-	@XmlElement(name="taskPath")
-	private String path;
-	@XmlElement(name="searchThreshold")
-	private int threshold = DataImport.THRESHOLD;
-	@XmlElement(name="taskInfo")
-	private Task.Info info = Task.Info.IDLE;
-	@XmlElement(name="taskExec")
-	private Task.Exec exec = Task.Exec.NO_OPERATION;
-	
-	@XmlElement(name="messageError")
-	private String error;
-	@XmlElement(name="messageWarning")
-	private String warning;
-	
-	@XmlElement(name="mugimugiBId")
-	private Integer mugimugi_bid;
-	@XmlElement(name="taskBook")
-	private Integer book;
-	
-	@XmlElement(name="duplicateList")
-	private Set<Integer> duplicate_list;
-	@XmlElement(name="mugimugiList")
-	private Set<Integer> mugimugi_list;
-
 	@Override
 	public boolean equals(Object obj) {
 		if(obj == null)
@@ -81,100 +56,20 @@ abstract class Task
 			return ((Task)obj).id.equals(id);
 	}
 
-	public int getThreshold() {
-		return threshold;
-	}
-
-	public void setThreshold(int threshold) {
-		this.threshold = threshold;
-	}
-
-	public Task.Info getInfo() {
-		return info;
-	}
-
-	public void setInfo(Task.Info info) {
-		this.info = info;
-	}
-
-	public Task.Exec getExec() {
-		return exec;
-	}
-
-	public void setExec(Task.Exec exec) {
-		this.exec = exec;
-	}
-
-	public boolean isSelected() {
-		return selected;
-	}
-
-	public void setSelected(boolean selected) {
-		this.selected = selected;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getPath() {
-		return path;
+	@Override
+	public int hashCode() {
+		return id.hashCode();
 	}
 	
-	public Integer getBook() {
-		return book;
+	private static class CDATAAdapter extends XmlAdapter<String, String>
+	{
+	    @Override
+	    public String marshal(String str) throws Exception {
+	        return "<![CDATA[" + str + "]]>";
+	    }
+	    @Override
+	    public String unmarshal(String str) throws Exception {
+	        return str;
+	    }
 	}
-	
-	protected void setBook(Integer book) {
-		this.book = book;
-	}
-	
-	public Integer getMugimugiBid() {
-		return mugimugi_bid;
-	}
-	
-	protected void setMugimugiBid(Integer mugimugi_bid) {
-		this.mugimugi_bid = mugimugi_bid;
-	}
-	
-	public Set<Integer> getDuplicatelist() {
-		return duplicate_list;
-	}
-
-	protected void setDuplicateList(Set<Integer> duplicate_list) {
-		this.duplicate_list = duplicate_list;
-	}
-	
-	public Set<Integer> getMugimugiList() {
-		return mugimugi_list;
-	}
-
-	protected void setMugimugiList(Set<Integer> mugimugi_list) {
-		this.mugimugi_list = mugimugi_list;
-	}
-	
-	public String getError() {
-		return error;
-	}
-	
-	protected void setError(String error) {
-		this.error = error;
-	}
-	
-	public String getWarning() {
-		return warning;
-	}
-	
-	protected void setWarning(String warning) {
-		this.warning = warning;
-	}
-
-	public abstract boolean isRunning();
-	
-	public abstract String getMessage();
-	
-	public abstract int getProgress();
-	
-	private transient boolean selected = false;
-
 }

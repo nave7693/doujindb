e72bf8db3033a166f85a3376440c4116dbcf2c10
loli10K <ezomori.nozomu@gmail.com>
2014-12-08 10:46:17 +0100

    Change Metadata to be used with JAXB

--- src/org/dyndns/doujindb/plug/impl/dataimport/Metadata.java
+++ src/org/dyndns/doujindb/plug/impl/dataimport/Metadata.java
@@ -1,18 +1,44 @@
 package org.dyndns.doujindb.plug.impl.dataimport;
 
-interface Metadata
+import java.util.*;
+
+import javax.xml.bind.annotation.*;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(namespace="org.dyndns.doujindb.plug.impl.dataimport", name="Metadata")
+@XmlSeeAlso({
+	org.dyndns.doujindb.plug.impl.dataimport.MugiMugiProvider.Metadata.class,
+})
+abstract class Metadata
 {
-	public String name();
-	public Iterable<String> alias();
-	public String translation();
-	public Integer pages();
-	public Long timestamp();
-	public String type();
-	public Long size();
-	public Iterable<String> artist();
-	public Iterable<String> circle();
-	public String convention();
-	public Iterable<String> content();
-	public Iterable<String> parody();
-	public String uri();
+	// Needed by JAXB
+	// Define this or suffer an IllegalAnnotationsException : Task does not have a no-arg default constructor.
+	Metadata() { }
+
+	@XmlElement(name="name")
+	protected String name;
+	@XmlElement(name="alias")
+	protected Set<String> alias = new HashSet<String>();
+	@XmlElement(name="translation")
+	protected String translation;
+	@XmlElement(name="pages")
+	protected Integer pages;
+	@XmlElement(name="timestamp")
+	protected Long timestamp;
+	@XmlElement(name="type")
+	protected String type;
+	@XmlElement(name="size")
+	protected Long size;
+	@XmlElement(name="artist")
+	protected Set<String> artist = new HashSet<String>();
+	@XmlElement(name="circle")
+	protected Set<String> circle = new HashSet<String>();
+	@XmlElement(name="circle")
+	protected String convention;
+	@XmlElement(name="content")
+	protected Set<String> content = new HashSet<String>();
+	@XmlElement(name="parody")
+	protected Set<String> parody = new HashSet<String>();
+	@XmlElement(name="uri")
+	protected String uri;
 }

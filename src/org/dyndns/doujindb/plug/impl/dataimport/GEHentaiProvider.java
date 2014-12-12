adc2a6f2831fdc4ef7f515bcf71ff96cd27dd05f
loli10K <ezomori.nozomu@gmail.com>
2014-12-12 23:00:29 +0100

    Add g.e-hentai.org Metadata Provider stub

---
+++ src/org/dyndns/doujindb/plug/impl/dataimport/GEHentaiProvider.java
@@ -0,0 +1,26 @@
+package org.dyndns.doujindb.plug.impl.dataimport;
+
+import java.io.File;
+import java.net.URI;
+
+final class GEHentaiProvider extends MetadataProvider {
+
+	@Override
+	public Metadata query(File image) throws TaskException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Metadata query(String name) throws TaskException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Metadata query(URI uri) throws TaskException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

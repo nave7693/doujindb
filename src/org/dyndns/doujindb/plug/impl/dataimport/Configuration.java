c06fb5c702e9f41c81cfc20d3c1bbd9741905f3f
loli10K <ezomori.nozomu@gmail.com>
2014-12-13 14:35:03 +0100

    Move autoresize Configuration option from MugiMugi to global scope

--- src/org/dyndns/doujindb/plug/impl/dataimport/Configuration.java
+++ src/org/dyndns/doujindb/plug/impl/dataimport/Configuration.java
@@ -7,8 +7,8 @@ public final class Configuration
 	public static final ConfigurationItem<String> options_http_useragent = new ConfigurationItem<String>("Mozilla/5.0 (compatible; " + DataImport.Name+ "/" + DataImport.Version + "; +" + DataImport.Weblink + ")", "User-Agent used in HTTP requests");
 	public static final ConfigurationItem<Integer> options_http_timeout = new ConfigurationItem<Integer>(15000, "Timeout value for HTTP connections expressed in milliseconds");
 	public static final ConfigurationItem<Boolean> options_autoadd = new ConfigurationItem<Boolean>(true , "Automatically add new entries (Arist, Circle, Parody ...) to the Database without any confirmation");
+	public static final ConfigurationItem<Boolean> options_autoresize = new ConfigurationItem<Boolean>(true , "Resize covers automatically before upload");
 	
 	public static final ConfigurationItem<String> provider_mugimugi_apikey = new ConfigurationItem<String>("", "User KEY needed to query doujinshi.mugimugi.org API system");
 	public static final ConfigurationItem<Integer> provider_mugimugi_threshold = new ConfigurationItem<Integer>(75, "Threshold limit for matching cover queries");
-	public static final ConfigurationItem<Boolean> provider_mugimugi_autoresize = new ConfigurationItem<Boolean>(true , "Resize covers automatically before upload");
 }

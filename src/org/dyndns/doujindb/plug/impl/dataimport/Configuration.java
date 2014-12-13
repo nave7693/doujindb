ed1da1d57c5d2c19ca3101619cabadf48c535c38
loli10K <ezomori.nozomu@gmail.com>
2014-12-13 14:47:32 +0100

    Add Configuration option 'autocrop'

--- src/org/dyndns/doujindb/plug/impl/dataimport/Configuration.java
+++ src/org/dyndns/doujindb/plug/impl/dataimport/Configuration.java
@@ -8,6 +8,7 @@ public final class Configuration
 	public static final ConfigurationItem<Integer> options_http_timeout = new ConfigurationItem<Integer>(15000, "Timeout value for HTTP connections expressed in milliseconds");
 	public static final ConfigurationItem<Boolean> options_autoadd = new ConfigurationItem<Boolean>(true , "Automatically add new entries (Arist, Circle, Parody ...) to the Database without any confirmation");
 	public static final ConfigurationItem<Boolean> options_autoresize = new ConfigurationItem<Boolean>(true , "Resize covers automatically before upload");
+	public static final ConfigurationItem<Boolean> options_autocrop = new ConfigurationItem<Boolean>(true , "Crop covers automatically before upload");
 	
 	public static final ConfigurationItem<String> provider_mugimugi_apikey = new ConfigurationItem<String>("", "User KEY needed to query doujinshi.mugimugi.org API system");
 	public static final ConfigurationItem<Integer> provider_mugimugi_threshold = new ConfigurationItem<Integer>(75, "Threshold limit for matching cover queries");
